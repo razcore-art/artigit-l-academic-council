87ef981c551c54994048209e62e20d484dadce03
Răzvan C. Rădulescu <razcore.art@gmail.com>
2021-01-21 22:02:32 +0200

    Add development/production environment

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,11 @@
 /* global BABYLON, anime */
-
 function randomUnicode () {
   const min = 0x21
   const max = 0x7e
   return String.fromCharCode(min + (max - min + 1) * Math.random())
 }
 
-function main () {
+function main (name, env) {
   const canvas = document.getElementById('babylon-engine')
   const babylon = new BABYLON.Engine(canvas, true, {
     preserveDrawingBuffer: true,
@@ -22,12 +21,63 @@ function main () {
   camera.setTarget(BABYLON.Vector3.Zero())
   camera.attachControl(canvas, true)
 
-  fetch('/assets/particleSystem.json')
+  let path = '/assets/particleSystem.json'
+  if (env === 'production') path = `/${name}$path`
+
+  fetch(path)
     .then((response) => response.json())
     .then((data) => {
       const particleSystem = BABYLON.ParticleSystem.Parse(data, scene, '')
       particleSystem.activeParticleCount = particleSystem.manualEmitCount
       particleSystem.minLifeTime = Number.MAX_SAFE_INTEGER
+
+      const duration = 6
+      const titleText = 'digital twin'
+      const title = document.getElementById('title')
+      title.append(
+        ...Array.from(titleText).map((value) => {
+          const span = document.createElement('span')
+          span.innerText = value
+          return span
+        })
+      )
+      let update = 0
+      const updates = Array.from(titleText).map(() => anime.random(10, 20))
+      const durations = Array.from(titleText).map(
+        () => 100 * anime.random(20, 10 * duration)
+      )
+
+      anime
+        .timeline()
+        .add({
+          targets: '#title',
+          duration: (1000 * duration) / 4,
+          opacity: [0, 1],
+          easing: 'easeInSine'
+        })
+        .add(
+          {
+            targets: '#title > span',
+            duration: 1000 * duration,
+            update: function (anim) {
+              update++
+              anim.animatables.forEach((animatable, index) => {
+                if (
+                  update % updates[index] === 0 &&
+                  anim.currentTime < durations[index]
+                ) {
+                  animatable.target.innerText = randomUnicode()
+                } else if (anim.currentTime >= durations[index]) {
+                  animatable.target.innerText = titleText.slice(
+                    index,
+                    index + 1
+                  )
+                }
+              })
+            }
+          },
+          0
+        )
     })
     .catch(console.error)
 
@@ -62,51 +112,4 @@ function main () {
 
   babylon.runRenderLoop(() => scene.render())
   window.addEventListener('resize', () => babylon.resize())
-
-  const duration = 6
-  const titleText = 'digital twin'
-  const title = document.getElementById('title')
-  title.append(
-    ...Array.from(titleText).map((value) => {
-      const span = document.createElement('span')
-      span.innerText = value
-      return span
-    })
-  )
-  let update = 0
-  const updates = Array.from(titleText).map(() => anime.random(10, 20))
-  const durations = Array.from(titleText).map(
-    () => 100 * anime.random(20, 10 * duration)
-  )
-
-  anime
-    .timeline()
-    .add({
-      targets: '#title',
-      duration: (1000 * duration) / 4,
-      opacity: [0, 1],
-      easing: 'easeInSine'
-    })
-    .add(
-      {
-        targets: '#title > span',
-        duration: 1000 * duration,
-        update: function (anim) {
-          update++
-          anim.animatables.forEach((animatable, index) => {
-            if (
-              update % updates[index] === 0 &&
-              anim.currentTime < durations[index]
-            ) {
-              animatable.target.innerText = randomUnicode()
-            } else if (anim.currentTime >= durations[index]) {
-              animatable.target.innerText = titleText.slice(index, index + 1)
-            }
-          })
-        }
-      },
-      0
-    )
 }
-
-main()
