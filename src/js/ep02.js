5d346ad21118cf5eccd5b073905b70b4e3ec6790
Răzvan C. Rădulescu <razcore.art@gmail.com>
2021-02-12 11:16:48 +0200

    Update to ep03

--- src/js/ep02.js
+++ src/js/ep02.js
@@ -1,3 +1,21 @@
+/* global Webcam */
+
 function main (baseURL) {
-  console.log(sessionStorage.ep01)
+  const webcam = new Webcam(
+    document.getElementById('webcam'),
+    'user',
+    document.getElementById('canvas')
+  )
+
+  webcam
+    .start()
+    .then(() => {
+      const snapshot = document.getElementById('snapshot')
+      snapshot.onclick = () => {
+        webcam.snap()
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+    })
 }
